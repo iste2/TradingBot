eaece34f14a685d342e4f7e1a879259713b4eed8
Lukas Thomas Nadenau <lukas.nadenau@rwth-aachen.de>
2023-04-01 21:53:54 +0200

    Unittest erweitert

--- TradingBot.Tests/MovingAverageConvergenceDivergenceIndicatorToolTests.cs
+++ TradingBot.Tests/MovingAverageConvergenceDivergenceIndicatorToolTests.cs
@@ -45,5 +45,6 @@ public class MovingAverageConvergenceDivergenceIndicatorToolTests
 
         // Assert
         Assert.That(hSignals, Has.Count.EqualTo(1));
+        Assert.That(hSignals.FirstOrDefault()!.ToolSignalKind, Is.EqualTo(ToolSignalKind.Buy));
     }
 }
